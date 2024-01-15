e75fd1a2c663152916bd9232e38cb6fc7ea8cbde
ttran375 <ttran375@my.centennialcollege.ca>
2024-01-15 19:01:15 +0000

    Mon, Jan 15, 2024, 7:01 PM +00:00

---
+++ week-1/Date/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Week_01_lab_02_Cars_W
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+        }
+    }
+
+
+    public class Date
+    {
+        private int year;
+        private int month;
+        private int day;
+
+        public Date(int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        
+    }
+}
