219aec514564cfca869a7ec89991c2d116826a59
ttran375 <ttran375@my.centennialcollege.ca>
2024-01-15 18:55:27 +0000

    Mon, Jan 15, 2024, 6:55 PM +00:00

--- week-1/Cars/Program.cs
+++ week-1/Cars/Program.cs
@@ -7,3 +7,21 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+
+class Car
+{
+    private int year;
+    private string model;
+    private bool isDrivable;
+    private double price;
+
+    public Car(int year, string model, bool isDrivable = true, double price)
+        {
+            this.year = year;
+            this.model = model;
+            this.isDrivable = isDrivable;
+            this.price = price;
+        }
+
+    
+}
