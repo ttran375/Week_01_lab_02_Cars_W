b64b3f5c2760c548237723cb1db4608bba430662
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-17 17:02:15 +0000

    Week_02_lab_06_Medal_W

--- Week_02_lab_06_Medal_W/Program.cs
+++ Week_02_lab_06_Medal_W/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -6,54 +8,53 @@ class Program
 {
     static void Main()
     {
-        // Create a medal object
-        Medal m1 = new Medal("Horace Gwynne", "Boxing", "Gold", 2012, true);
-
-        // Print the object
+        //create a medal object
+        Medal m1 = new Medal("Horace Gwynne", "Boxing", MedalColor.Gold, 2012, true);
+        //print the object
         Console.WriteLine(m1);
-
-        // Print only the name of the medal holder
+        //print only the name of the medal holder
         Console.WriteLine(m1.Name);
 
-        // Create another object
-        Medal m2 = new Medal("Michael Phelps", "Swimming", "Gold", 2012, false);
 
-        // Print the updated m2
+        //create another object
+        Medal m2 = new Medal("Michael Phelps", "Swimming", MedalColor.Gold, 2012, false);
+        //print the updated m2
         Console.WriteLine(m2);
 
-        // Create a list to store the medal objects
+        //create a list to store the medal objects
         List<Medal> medals = new List<Medal>() { m1, m2 };
 
-        medals.Add(new Medal("Ryan Cochrane", "Swimming", "Silver", 2012, false));
-        medals.Add(new Medal("Adam van Koeverden", "Canoeing", "Silver", 2012, false));
-        medals.Add(new Medal("Rosie MacLennan", "Gymnastics", "Gold", 2012, false));
-        medals.Add(new Medal("Christine Girard", "Weightlifting", "Bronze", 2012, false));
-        medals.Add(new Medal("Charles Hamelin", "Short Track", "Gold", 2014, true));
-        medals.Add(new Medal("Alexandre Bilodeau", "Freestyle skiing", "Gold", 2012, true));
-        medals.Add(new Medal("Jennifer Jones", "Curling", "Gold", 2014, false));
-        medals.Add(new Medal("Charle Cournoyer", "Short Track", "Bronze", 2014, false));
-        medals.Add(new Medal("Mark McMorris", "Snowboarding", "Bronze", 2014, false));
-        medals.Add(new Medal("Sidney Crosby ", "Ice Hockey", "Gold", 2014, false));
-        medals.Add(new Medal("Brad Jacobs", "Curling", "Gold", 2014, false));
-        medals.Add(new Medal("Ryan Fry", "Curling", "Gold", 2014, false));
-        medals.Add(new Medal("Antoine Valois-Fortier", "Judo", "Bronze", 2012, false));
-        medals.Add(new Medal("Brent Hayden", "Swimming", "Bronze", 2012, false));
+        medals.Add(new Medal("Ryan Cochrane", "Swimming", MedalColor.Silver, 2012, false));
+        medals.Add(new Medal("Adam van Koeverden", "Canoeing", MedalColor.Silver, 2012, false));
+        medals.Add(new Medal("Rosie MacLennan", "Gymnastics", MedalColor.Gold, 2012, false));
+        medals.Add(new Medal("Christine Girard", "Weightlifting", MedalColor.Bronze, 2012, false));
+        medals.Add(new Medal("Charles Hamelin", "Short Track", MedalColor.Gold, 2014, true));
+        medals.Add(new Medal("Alexandre Bilodeau", "Freestyle skiing", MedalColor.Gold, 2012, true));
+        medals.Add(new Medal("Jennifer Jones", "Curling", MedalColor.Gold, 2014, false));
+        medals.Add(new Medal("Charle Cournoyer", "Short Track", MedalColor.Bronze, 2014, false));
+        medals.Add(new Medal("Mark McMorris", "Snowboarding", MedalColor.Bronze, 2014, false));
+        medals.Add(new Medal("Sidney Crosby ", "Ice Hockey", MedalColor.Gold, 2014, false));
+        medals.Add(new Medal("Brad Jacobs", "Curling", MedalColor.Gold, 2014, false));
+        medals.Add(new Medal("Ryan Fry", "Curling", MedalColor.Gold, 2014, false));
+        medals.Add(new Medal("Antoine Valois-Fortier", "Judo", MedalColor.Bronze, 2012, false));
+        medals.Add(new Medal("Brent Hayden", "Swimming", MedalColor.Bronze, 2012, false));
 
-        // Prints a numbered list of 16 medals
+
+        //prints a numbered list of 16 medals.
         Console.WriteLine("\n\nAll 16 medals");
         for (int i = 0; i < medals.Count; i++)
         {
             Console.WriteLine($"{i + 1}. {medals[i]}");
         }
 
-        // Prints a numbered list of 16 names (ONLY)
+        //prints a numbered list of 16 names (ONLY)
         Console.WriteLine("\n\nAll 16 names");
         for (int i = 0; i < medals.Count; i++)
         {
             Console.WriteLine($"{i + 1}. {medals[i].Name}");
         }
 
-        // Prints a numbered list of 9 gold medals
+        //prints a numbered list of 9 gold medals
         Console.WriteLine("\n\nAll 9 gold medals");
         int goldCount = 0;
         for (int i = 0; i < medals.Count; i++)
@@ -65,7 +66,7 @@ class Program
             }
         }
 
-        // Prints a numbered list of 9 medals in 2012
+        //prints a numbered list of 9 medals in 2012
         Console.WriteLine("\n\nAll 9 medals in 2012");
         int year2012Count = 0;
         for (int i = 0; i < medals.Count; i++)
@@ -77,7 +78,7 @@ class Program
             }
         }
 
-        // Prints a numbered list of 4 gold medals in 2012
+        //prints a numbered list of 4 gold medals in 2012
         Console.WriteLine("\n\nAll 4 gold medals in 2012");
         int gold2012Count = 0;
         for (int i = 0; i < medals.Count; i++)
@@ -89,7 +90,7 @@ class Program
             }
         }
 
-        // Prints a numbered list of 3 world record medals
+        //prints a numbered list of 3 world record medals
         Console.WriteLine("\n\nAll 3 records");
         int recordCount = 0;
         for (int i = 0; i < medals.Count; i++)
@@ -101,7 +102,7 @@ class Program
             }
         }
 
-        // Saving all the medals to file Medals.txt
+        //saving all the medals to file Medals.txt
         Console.WriteLine("\n\nSaving to file");
         using (StreamWriter writer = new StreamWriter("Medals.txt"))
         {
@@ -132,11 +133,11 @@ public class Medal
     public bool IsRecord { get; }
 
     // Constructor
-    public Medal(string name, string theEvent, string color, int year, bool isRecord)
+    public Medal(string name, string theEvent, MedalColor color, int year, bool isRecord)
     {
         Name = name;
         TheEvent = theEvent;
-        Color = Enum.Parse<MedalColor>(color, true);
+        Color = color;
         Year = year;
         IsRecord = isRecord;
     }
